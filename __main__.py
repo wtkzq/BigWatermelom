f3802435023e24403674dd804ad60f494896fe63
wtkzq <>
2024-02-07 20:12:54 +0800

    v0.2.1 - 日志显示详细信息

--- __main__.py
+++ __main__.py
@@ -2,6 +2,6 @@ import game
 from settings import settings
 import logging
 
-
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format="%(levelname)s (file %(filename)s, in %(funcName)s, line %(lineno)s, %(asctime)s): %("
+                           "message)s", level=logging.INFO)
 game.Game(settings).main_loop()
